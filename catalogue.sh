2103d7b966448bad011c0c830d731a719a1a69df
yamaha <venkateshsubhash.sv@gmail.com>
2024-05-06 01:20:46 +0530

    creating mongobd for roboshop

--- catalogue.sh
+++ catalogue.sh
@@ -17,3 +17,67 @@ then
 else
     echo -e "$G Your a root user $N"
 fi
+
+dnf module disable nodejs -y @>> $LOGFILE
+
+VALIDATE $? "disabling nodejs"
+
+dnf module enable nodejs:18 -y @>> $LOGFILE
+
+VALIDATE $? "Enabling nodejs"
+
+dnf install nodejs -y @>> $LOGFILE
+
+VALIDATE $? "installing nodejs"
+
+useradd roboshop @>> $LOGFILE
+
+VALIDATE $? "Creating user"
+
+mkdir -p /app @>> $LOGFILE
+
+VALIDATE $? "Creating directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip @>> $LOGFILE
+
+VALIDATE $? "downloading catalogue data"
+
+cd /app @>> $LOGFILE
+
+VALIDATE $? "entering directory"
+
+dnf module enable nodejs:18 -y @>> $LOGFILE
+
+VALIDATE $? "unzipping the dependencies"
+
+npm install @>> $LOGFILE
+
+VALIDATE $? "installing the dependencies"
+
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service @>> $LOGFILE
+
+VALIDATE $? "copying catalogue service"
+
+systemctl daemon-reload @>> $LOGFILE
+
+VALIDATE $? "daemon reloading"
+
+systemctl enable catalogue @>> $LOGFILE
+
+VALIDATE $? "enabling catalogue"
+
+systemctl start catalogue @>> $LOGFILE
+
+VALIDATE $? "starting catalogue"
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "copying mongodb repo"
+
+dnf install mongodb-org-shell -y
+
+VALIDATE $? "installing mongodb repo"
+
+mongo --host mongodb.erumamadu.online </app/schema/catalogue.js
+
+VALIDATE $? "Loading catalogue data into mongodb"
