c45a4d90865f9e112dd3f2d28d4e098007e7daa2
yamaha <venkateshsubhash.sv@gmail.com>
2024-05-09 14:51:51 +0530

    configuring web

--- shipping.sh
+++ shipping.sh
@@ -4,7 +4,7 @@ ID=$( id -u )
 
 TIMESTAMP=$(date +%F-%H-%M-%S)
 
-LOGFILE="/tmp/$0-$TIMESTAMP"
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 R="\e[31m"
 G="\e[32m"
@@ -27,53 +27,53 @@ else
     echo -e "$G Your a root user $N"
 fi
 
-dnf install maven -y
+dnf install maven -y &>> $LOGFILE
 VALIDATE $? "Installing Maven"
 
-id roboshop
+id roboshop &>> $LOGFILE
 if [ $? -ne 0 ]
 then
-    useradd roboshop
+    useradd roboshop &>> $LOGFILE
     VALIDATE $? "Creating roboshop user"
 else
     echo -e "user is already existing ....$Y SKIPPING $N"
 fi
 
-mkdir -p /app
+mkdir -p /app &>> $LOGFILE
 VALIDATE $? "creating app directory"
 
-curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip
+curl -L -o /tmp/shipping.zip https://roboshop-builds.s3.amazonaws.com/shipping.zip &>> $LOGFILE
 VALIDATE $? "downloading shipping file"
 
-cd /app
+cd /app &>> $LOGFILE
 VALIDATE $? "moving to app directory"
 
-unzip -o /tmp/shipping.zip
+unzip -o /tmp/shipping.zip &>> $LOGFILE
 VALIDATE $? "unzipping shipping"
 
-mvn clean package
+mvn clean package &>> $LOGFILE
 VALIDATE $? "Installing dependencies"
 
-mv target/shipping-1.0.jar shipping.jar
+mv target/shipping-1.0.jar shipping.jar &>> $LOGFILE
 VALIDATE $? "renaming jar file"
 
-cp /home/centos/roboshop.shell/shipping.service /etc/systemd/system/shipping.service
+cp /home/centos/roboshop.shell/shipping.service /etc/systemd/system/shipping.service &>> $LOGFILE
 VALIDATE $? "Copying Shipping service"
 
-systemctl daemon-reload
+systemctl daemon-reload &>> $LOGFILE
 VALIDATE $? "daemon reloding"
 
-systemctl enable shipping 
+systemctl enable shipping  &>> $LOGFILE
 VALIDATE $? "enabling shipping"
 
-systemctl start shipping
+systemctl start shipping &>> $LOGFILE
 VALIDATE $? "starting shipping"
 
-dnf install mysql -y
+dnf install mysql -y &>> $LOGFILE
 VALIDATE $? "Installing mysql"
 
-mysql -h mysql.erumamadu.online -uroot -pRoboShop@1 < /app/schema/shipping.sql 
+mysql -h mysql.erumamadu.online -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE
 VALIDATE $? "Loading data into mysql"
 
-systemctl restart shipping
+systemctl restart shipping &>> $LOGFILE
 VALIDATE $? "restarting shipping"
