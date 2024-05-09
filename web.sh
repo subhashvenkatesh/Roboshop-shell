3d452b7ea9c7a6d7674d4ea378e9ff71dc380373
yamaha <venkateshsubhash.sv@gmail.com>
2024-05-09 12:29:30 +0530

    configuring web

--- web.sh
+++ web.sh
@@ -12,7 +12,7 @@ Y="\e[33m"
 N="\e[0m"
 
 VALIDATE(){
-if [ $1 -ne 0]
+if [ $1 -ne 0 ]
 then
     echo -e "$2.....$R FAILED $N"
 else
@@ -28,29 +28,29 @@ else
     echo -e "$G Your a root user $N"
 fi   
 
-dnf install nginx -y
+dnf install nginx -y &>> $LOGFILE
 VALIDATE $? "Installing nginx"
 
-systemctl enable nginx
+systemctl enable nginx &>> $LOGFILE
 VALIDATE $? "enabling nignx"
 
-systemctl start nginx
+systemctl start nginx  &>> $LOGFILE
 VALIDATE $? "starting nginx"
 
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/*  &>> $LOGFILE
 VALIDATE $? "removing default website"
 
-curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip
+curl -o /tmp/web.zip https://roboshop-builds.s3.amazonaws.com/web.zip &>> $LOGFILE
 VALIDATE $? "downloading web content"
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>> $LOGFILE
 VALIDATE $? "moving html directory"
 
-unzip -o /tmp/web.zip
+unzip -o /tmp/web.zip  &>> $LOGFILE
 VALIDATE $? "unzipping web"
 
-cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp /home/centos/roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf &>> $LOGFILE
 VALIDATE $? "copying roboshop revers proxy config"
 
-systemctl restart nginx 
+systemctl restart nginx  &>> $LOGFILE
 VALIDATE $? "restart nginx"
