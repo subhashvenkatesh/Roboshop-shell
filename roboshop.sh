8d821ec856e4cb7e9370198b1efb4acaa520aaf2
yamaha <venkateshsubhash.sv@gmail.com>
2024-05-18 18:26:56 +0530

    EC2-automating

--- roboshop.sh
+++ roboshop.sh
@@ -5,7 +5,8 @@ AMI=ami-0f3c7d07486cad139
 SG_ID=sg-09551652be94e0876
 
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-
+ZONE_ID=Z04745361SSM2LZ7ZKXFN
+DOMAIN_NAME=erumamadu.online
 for i in "${INSTANCES[@]}"
 do
     
@@ -17,4 +18,24 @@ do
     fi
     IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i=$IP_ADDRESS"
+
+    aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch "
+    {
+    "Comment": "Testing creating a record set"
+    ,"Changes": [{
+      "Action"              : "CREATE"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "$i.$DOMAIN_NAME"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "$IP_ADDRESS"
+        }]
+      }
+    }]
+  }
+  "
+
 done
