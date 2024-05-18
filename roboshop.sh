8ba9bcae5f9e9c248a329d498887e78e96d11f8f
yamaha <venkateshsubhash.sv@gmail.com>
2024-05-18 17:47:10 +0530

    EC2-automating

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ INSTANCES=$("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "ship
 
 for i in "${INSTANCES[@]}"
 do
-    echo "instance is:: $i""
+    echo "instance is:: $i"
     if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
     then
         INSTANCE_TYPE="t3.small"
